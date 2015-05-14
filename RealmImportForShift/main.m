79451a90d59fbd67dd3d8a19767061e33165fb47
hiratsukashu <hiratsukashu@indival.co.jp>
2015-05-14 11:46:33 +0900

    Initial Commit

---
+++ RealmImportForShift/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RealmImportForShift
+//
+//  Created by 平塚 俊輔 on 2015/05/14.
+//  Copyright (c) 2015年 平塚 俊輔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
